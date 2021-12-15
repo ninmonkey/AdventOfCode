6474bec2a8e2403432223fce98f73419f7fc0053
Jake Bolton <ninmonkeys@gmail.com>
2021-12-14 21:32:01 -0600

    pqlib: New functions: Inspect metadata

--- Day1/day1.ps1
+++ Day1/day1.ps1
@@ -1,5 +1,10 @@
-﻿goto 'C:\Users\cppmo_000\SkyDrive\Documents\2021\My_Github\AdventOfCode'
-$root = 'C:\Users\cppmo_000\SkyDrive\Documents\2021\My_Github\AdventOfCode\utils'
+﻿$App = @{ Root = Get-Item $PSScriptRoot }
+$App += @{
+    Utils = Join-Path $App.Root 'utils'
+}
+# goto  'C:\Users\cppmo_000\SkyDrive\Documents\2021\My_Github\AdventOfCode'
+# $root = 'C:\Users\cppmo_000\SkyDrive\Documents\2021\My_Github\AdventOfCode\utils'
+$root = $App.Utils
 if (!($env:PSModulePath -split ';' -match (relit $root)) ) {
     $env:PSModulePath += ';', $root
     $root | Label 'Added to $Env:PSModulePath'
